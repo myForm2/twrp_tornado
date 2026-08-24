6e2ddb408f4420def480859e29e1a16ad0adf678
heppy <nitusingh5457@gmail.com>
2026-08-24 21:17:08 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,4 +197,4 @@ TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 # Use legacy code for battery readout
 TW_USE_LEGACY_BATTERY_SERVICES := true
 TW_LOAD_VENDOR_BOOT_MODULES := true
-TW_DEVICE_VERSION := @Qbhi_@heppysingh
+TW_DEVICE_VERSION := @heppysingh
